58b685022fec2174e0a8ad9cb0b92c3828fc34c2
Matt <mattsandovalb@gmail.com>
2023-02-08 17:34:56 +0100

    Cards, controllers, migrations and Route

---
+++ app/Http/Controllers/CardController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Card;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Card::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $card = new Card();
+
+        $request->validate([
+            'title'=>'required',
+            'description'=>'required',
+            'client' => 'required',
+        ]);
+
+        $card->title = $request->title;
+        $card->description = $request->description;
+        $card->client = $request->client;
+
+        $card->save();
+        return  $card;
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Card  $card
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Card $card, $id)
+    {
+        $card = Card::find($id);
+        return $card;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Card  $card
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Card $card, $id)
+    {
+        $card = Card::find($id);
+
+        $card->title = $request->title;
+        $card->description = $request->description;
+        $card->client = $request->client;
+
+        $card->update();
+        return  $card;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Card  $card
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Card $card, $id)
+    {
+        $card = Card::find($id);
+
+        $card->delete();
+
+    }
+}
